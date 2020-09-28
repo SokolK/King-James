761d919e1a95b6c7465549d9abb9ae21a400d916
SokolK <47599824+SokolK@users.noreply.github.com>
2020-09-28 11:29:32 +0200

    Updates

---
+++ precache-manifest.190025ec5a94bc3035874c0203248576.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "49fea0b66202535d1a7ba5686a8a49ba",
+    "url": "/King-James/index.html"
+  },
+  {
+    "revision": "e354661180c801a39960",
+    "url": "/King-James/static/css/main.5423e770.chunk.css"
+  },
+  {
+    "revision": "2ffb0a3ad5b2e37d1bb4",
+    "url": "/King-James/static/js/2.b49b9114.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/King-James/static/js/2.b49b9114.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "e354661180c801a39960",
+    "url": "/King-James/static/js/main.381242ae.chunk.js"
+  },
+  {
+    "revision": "3aa82457cbeec259f799",
+    "url": "/King-James/static/js/runtime-main.ddc84664.js"
+  }
+]);
